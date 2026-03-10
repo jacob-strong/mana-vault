8902f8c8449354875c4d9437d8126b8712e101d5
jacob-strong <104380832+jacob-strong@users.noreply.github.com>
2026-03-09 18:30:41 -0600

    a functional site

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import CardListGroup from "./components/CardListGroup";
 function App() {
   return (
     <div className="container mt-4">
-      <h1 className="mb-3">My List</h1>
       <CardListGroup />
     </div>
   );
